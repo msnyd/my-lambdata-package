64657a7e3ca9f41671a95d81353135622127a6fe
msnyd <46577534+msnyd@users.noreply.github.com>
2020-02-11 12:49:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     version="1.3",
     author="M Snyder",
     author_email="msnyder97@gmail.com",
-    description="Helps with splitting data",
+    description="Useful data science functions",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     license="",
     url="https://github.com/msnyd/my-lambdata-package",
     keywords="data-split-test",
-    packages=find_packages() # ["game_utils"]
+    packages=find_packages()
 )
